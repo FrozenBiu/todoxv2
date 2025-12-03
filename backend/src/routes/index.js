484544967c72c7432c3ace335c9e75cade3b4ae0
BiuBiu <tieutruongkhanh@gmail.com>
2025-12-03 22:38:39 +0700

    update ping route for backend

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,9 @@
 import tasksRouter from "./tasks.js";
+import pingRouter from "./ping.js";
 
 function route(app) {
   app.use("/api/tasks", tasksRouter);
+  app.use("/api/ping", pingRouter);
 }
 
 export default route;
